214577a90a06f45432b8c02e95c888d37317eadd
khacthiennguyen <khacthien.brand@gmail.com>
2025-05-03 19:16:24 +0700

    fix detection

--- routes/detection.js
+++ routes/detection.js
@@ -27,6 +27,7 @@ const upload = multer({
   storage: storage,
   limits: { fileSize: 10 * 1024 * 1024 }, // 10MB limit
   fileFilter: (req, file, cb) => {
+    // console.log("Received file mimetype:", file.mimetype);
     // Accept only images
     if (file.mimetype.startsWith('image/')) {
       cb(null, true)
